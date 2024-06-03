5f730972094cc3d1b5ad4ce205f0daef11ffee90
Eldar Faaiz <iniakunbisnisku@gmail.com>
2024-06-03 23:21:07 +0700

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,8 +12,8 @@ export default function Home() {
         <meta property="fc:frame" content="vNext"/>
         <meta name="fc:frame:image" content={frameImgUrl}/>
         <meta name="fc:frame:image:aspect_ratio" content="1:1"/>
-        <meta property="fc:frame:button:1" content="Mint NFT"/>
-        <meta property="fc:frame:button:1:action" content="tx"/>
+        <meta name="fc:frame:button:1" content="Mint NFT"/>
+        <meta name="fc:frame:button:1:action" content="tx"/>
         <meta 
           property="fc:frame:button:1:target"
           content={`${HOST_URL}/api/frame/op/get-tx-frame`}
